54f14e1bf6a2b142005cb32d647f77466790240d
David Ekunno <matramere@gmail.com>
2023-07-13 23:12:11 -0500

    navbar update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom"
 
 export function Nav () {
     return (
-        <div>
-            <h3>Nav bar!</h3>
+        <div className="black">
+            <Link className="link" to="/">Home</Link>
+            <Link className="link margy" to="/page2">2nd Page</Link>
+            <Link className="link" to="/page3">3rd Page</Link>
         </div>
     )
 }
