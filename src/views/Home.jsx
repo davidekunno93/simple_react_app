556c98dec8b1561bccb08245c686681a6cce9083
David Ekunno <matramere@gmail.com>
2023-08-03 22:28:27 -0500

    updated home page

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,11 @@
 
 const Home = () => {
     return (
-        <h1>This is Home!</h1>
+        <>
+        <h1 className="center-text">This is Home!</h1>
+        <h2 className="center-text">Shop our products in the products section!</h2>
+        <img className="bowser center" src="/images/bowser.png" />
+        </>
     )
 }
 export default Home
